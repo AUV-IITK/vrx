e9916aa21941eb16c46df4a0fddc3a5923fafe58
Ayush Gupta <7ayushgupta@gmail.com>
2019-09-08 22:57:08 +0530

    Added basic mapping

--- wamv_controls/cascaded_pid_controller/setup.py
+++ wamv_controls/cascaded_pid_controller/setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['cascaded_pid_controller'],
+    packages=['PID'],
     package_dir={'': 'src'},
     requires=['rospy']
 )
